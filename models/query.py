9ce12edd266f7f7bea58cfcfaee81cf426b6a953
Ismail Pelaseyed <homanp@gmail.com>
2024-01-16 09:48:38 -0800

    Fix query response model

--- models/query.py
+++ models/query.py
@@ -9,6 +9,12 @@ class RequestPayload(BaseModel):
     index_name: str
 
 
+class ResponseData(BaseModel):
+    content: str
+    file_url: str
+    page_label: str
+
+
 class ResponsePayload(BaseModel):
     success: bool
-    data: List
+    data: List[ResponseData]

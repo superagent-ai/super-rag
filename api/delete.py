84e1e35c2a7c756e78ed15bda16bc1be591d60cb
Ismail Pelaseyed <homanp@gmail.com>
2024-01-17 17:28:33 -0800

    Add authentication

--- api/delete.py
+++ api/delete.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.delete import RequestPayload, ResponsePayload
 from service.vector_database import get_vector_service, VectorService
+from auth.user import get_current_api_user
 
 router = APIRouter()
 
 
 @router.post("/delete", response_model=ResponsePayload)
-async def delete(payload: RequestPayload):
+async def delete(payload: RequestPayload, _api_user=Depends(get_current_api_user)):
     vector_service: VectorService = get_vector_service(
         index_name=payload.index_name, credentials=payload.vector_database
     )
--- api/ingest.py
+++ api/ingest.py
@@ -1,13 +1,16 @@
 from typing import Dict
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from models.ingest import RequestPayload
 from service.embedding import EmbeddingService
+from auth.user import get_current_api_user
 
 router = APIRouter()
 
 
 @router.post("/ingest")
-async def ingest(payload: RequestPayload) -> Dict:
+async def ingest(
+    payload: RequestPayload, _api_user=Depends(get_current_api_user)
+) -> Dict:
     embedding_service = EmbeddingService(
         files=payload.files,
         index_name=payload.index_name,

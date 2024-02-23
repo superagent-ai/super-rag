90729f16e4099425611e0490f3a50a50114dec3b
Ismail Pelaseyed <homanp@gmail.com>
2024-02-22 20:45:06 -0800

    Automatically detect file type

--- models/file.py
+++ models/file.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from urllib.parse import urlparse
+from urllib.parse import unquote
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class FileType(Enum):
@@ -11,8 +13,39 @@ class FileType(Enum):
     md = "MARKDOWN"
     csv = "CSV"
     xlsx = "XLSX"
+    html = "HTML"
+    json = "JSON"
+
+    def suffix(self) -> str:
+        suffixes = {
+            "TXT": ".txt",
+            "PDF": ".pdf",
+            "MARKDOWN": ".md",
+            "DOCX": ".docx",
+            "CSV": ".csv",
+            "XLSX": ".xlsx",
+            "PPTX": ".pptx",
+            "HTML": ".html",
+            "JSON": ".json",
+        }
+        return suffixes[self.value]
 
 
 class File(BaseModel):
-    type: FileType
     url: str
+    type: FileType | None = None
+
+    @validator("type", pre=True, always=True)
+    def set_type_from_url(cls, v, values):
+        if v is not None:
+            return v
+        url = values.get("url")
+        if url:
+            parsed_url = urlparse(url)
+            path = unquote(parsed_url.path)
+            extension = path.split(".")[-1].lower()
+            try:
+                return FileType[extension]
+            except KeyError:
+                raise ValueError(f"Unsupported file type for URL: {url}")
+        return v

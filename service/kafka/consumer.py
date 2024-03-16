ee7dc655aa2c48f10176d1dae46a1b4a3fddcc7e
alisalim17 <alisalimli1899@gmail.com>
2024-03-16 11:14:29 +0400

    chore: add sasl password and username

--- service/kafka/consumer.py
+++ service/kafka/consumer.py
@@ -1,6 +1,7 @@
 import json
 
 from kafka import KafkaConsumer
+from decouple import config
 
 from service.kafka.config import kafka_bootstrap_servers
 
@@ -9,10 +10,14 @@ def get_kafka_consumer(topic: str):
     consumer = KafkaConsumer(
         topic,
         bootstrap_servers=kafka_bootstrap_servers,
-        auto_offset_reset="earliest",
-        enable_auto_commit=False,
-        value_deserializer=lambda m: json.loads(m.decode("ascii")),
         group_id="my-group",
+        security_protocol=config("KAFKA_SECURITY_PROTOCOL", "PLAINTEXT"),
+        sasl_mechanism=config("KAFKA_SASL_MECHANISM", "PLAIN"),
+        sasl_plain_username=config("KAFKA_SASL_PLAIN_USERNAME", None),
+        sasl_plain_password=config("KAFKA_SASL_PLAIN_PASSWORD", None),
+        auto_offset_reset="earliest",
+        value_deserializer=lambda m: json.loads(m.decode("ascii")),
+        enable_auto_commit=False,
     )
 
     return consumer

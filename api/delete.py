4ba341838fc95b55132b2ffd204210ba1c08ed19
alisalim17 <alisalimli1899@gmail.com>
2024-03-27 10:42:00 +0400

    feat: delete summary index too

--- api/delete.py
+++ api/delete.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from models.delete import RequestPayload, ResponsePayload
 from vectordbs import get_vector_service
 from vectordbs.base import BaseVectorDatabase
+from utils.summarise import SUMMARY_SUFFIX
 
 router = APIRouter()
 
@@ -16,8 +17,15 @@ async def delete(payload: RequestPayload):
         encoder=encoder,
         dimensions=payload.encoder.dimensions,
     )
+    summary_vector_service: BaseVectorDatabase = get_vector_service(
+        index_name=f"{payload.index_name}{SUMMARY_SUFFIX}",
+        credentials=payload.vector_database,
+        encoder=encoder,
+        dimensions=payload.encoder.dimensions,
+    )
 
     for file in payload.files:
         data = await vector_service.delete(file_url=file.url)
+        await summary_vector_service.delete(file_url=file.url)
 
     return ResponsePayload(success=True, data=data)
